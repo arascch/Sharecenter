7cfc59614d38e71c9c6177db72d248ccec8a0307
aracch <arash.ask.94@gmail.com>
2020-04-03 08:22:32 -0700

    add logout page and view for dashboard

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate , login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == 'POST':
@@ -22,3 +23,7 @@ def user_login(request):
     else:
         form = LoginForm
     return render(request , 'account/login.html' , {'form':form})
+
+@login_required
+def dashboard(request):
+    return render(request , 'account/dashboard.html' , {'section':'dashboard'})
