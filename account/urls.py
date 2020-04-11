abfd127a2a46bd5db91a522c49eb0bf660f4e452
aracch <arash.ask.94@gmail.com>
2020-04-11 08:50:03 -0700

    add something

--- account/urls.py
+++ account/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('password_reset/done/' , auth_views.PasswordResetDoneView.as_view() , name = 'password_reset_done'),
     path('reset/<uidb64>/<token>/' , auth_views.PasswordResetConfirmView.as_view() , name = 'password_reset_confirm'),
     path('reset/done/' , auth_views.PasswordResetCompleteView.as_view() , name = 'password_reset_complete'),
-]
+    path('register/' , views.register , name = 'register'),
+    ]
