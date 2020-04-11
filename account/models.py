a107a8eb6064230bb3453d30a9fae4bd1e8f5af4
aracch <arash.ask.94@gmail.com>
2020-04-11 09:03:29 -0700

    correct some errors

--- account/models.py
+++ account/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 class profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank = True , null = True)
-    photo = models.ImageField(upload_to = 'user/%Y/%m/%d/' , blanl = True)
+    photo = models.ImageField(upload_to = 'user/%Y/%m/%d/' , blank = True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
-        
+
--- share_center/urls.py
+++ share_center/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls import static
 
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     path('account/' , include('account.urls')),
     path('' , include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
