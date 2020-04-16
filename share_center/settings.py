9310297d5b6300b28ceed34e2da95522f886fa0e
aracch <arash.ask.94@gmail.com>
2020-04-16 00:57:28 -0700

    q

--- share_center/settings.py
+++ share_center/settings.py
@@ -125,3 +125,8 @@ LOGOUT_URL= 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backend.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
