31db796208f2e83567e0173cfa91332c3daca2a7
aracch <arash.ask.94@gmail.com>
2020-04-25 03:25:19 -0700

    add format validation

--- images/forms.py
+++ images/forms.py
@@ -8,3 +8,11 @@ class ImageCreateForm(forms.ModelForm):
         widget = {
             'url' : forms.HiddenInput,
         }
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg' , 'jpeg']
+        extension = url.rsplit('.' , 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('the given url does not match valid image extensions.')
+        return url
